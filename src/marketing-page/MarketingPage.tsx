141ea5045b507a84a679938c913a4cea6f6c5dd6
retiredchicken <ivanotaz.io@gmail.com>
2026-03-23 03:43:30 +0800

    vercel issue fix

--- src/marketing-page/MarketingPage.tsx
+++ src/marketing-page/MarketingPage.tsx
@@ -42,7 +42,6 @@ export default function MarketingPage(props: { disableCustomTheme?: boolean }) {
         onUploadComplete={setUploadedPreviewUrl}
         onLocalPreviewChange={setLocalPreviewUrl}
         onPredictUiChange={setPredictUi}
-        onHeatmapStorageUrlChange={setHeatmapStorageUrl}
       />
       <div>
         {/* <LogoCollection /> */}
